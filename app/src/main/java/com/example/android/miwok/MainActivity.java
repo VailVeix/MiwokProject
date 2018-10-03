869f3a7036276ebf0d6f6fbbf75c7c82dffa8268
Alex Kasa <vailveix@gmail.com>
2018-10-03 15:34:08 -0400

    Quiz #17
    onClickListener use

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,33 +29,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view){
-        Intent intent = new Intent(this, NumbersActivity.class);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
-    }
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, NumbersActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
 
-    public void openColorsList(View view){
-        Intent intent = new Intent(this, ColorsActivity.class);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
-    }
+        TextView colors = (TextView) findViewById(R.id.colors);
+        colors.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, ColorsActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
 
-    public void openFamilyList(View view){
-        Intent intent = new Intent(this, FamilyActivity.class);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
-    }
+        TextView family = (TextView) findViewById(R.id.family);
+        family.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, FamilyActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
 
-    public void openPhrasesList(View view){
-        Intent intent = new Intent(this, PhrasesActivity.class);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, PhrasesActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
